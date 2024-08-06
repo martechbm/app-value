c7cde68cdc230dfa212f2dbfa5b7f4df28aba714
brmarmello <brmarmello@gmail.com>
2024-08-05 22:21:34 -0300

    Feat: create price calculator page using React Router.

---
+++ app/src/pages/CalculatorPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CalculatorForm from '../components/CalculatorForm';
+
+const CalculatorPage = () => {
+  return (
+    <div>
+      <h1>Quanto custa um aplicativo?</h1>
+      <CalculatorForm />
+    </div>
+  );
+};
+
+export default CalculatorPage;
